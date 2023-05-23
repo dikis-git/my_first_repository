1471aba833a0e2256cea2eec64c53d7a009a7832
Dmitry Kislykh <dmitrykislykh@gmail.com>
2023-05-23 18:30:06 +0500

    some desc

--- some_file.py
+++ some_file.py
@@ -1,2 +1,33 @@
+import requests
+import time
 
-print('Some Text')
+
+API_URL: str = 'https://api.telegram.org/bot'
+API_CATS_URL: str = 'https://api.thecatapi.com/v1/images/search'
+BOT_TOKEN: str = '1119055753:AAHwmgf-fXZ0IBkUjXm3SjgyDBUbSgZp2Yo'
+TEXT: str = 'Ура! Классный апдейт!'
+ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком :('
+
+offset: int = -2
+counter: int = 0
+cat_response: requests.Response
+cat_link: str
+
+
+while counter < 100:
+    print('attempt =', counter)
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+
+    time.sleep(1)
+    counter += 1
